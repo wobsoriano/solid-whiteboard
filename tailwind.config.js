a4b4f5d79790c4383a67dcb4d0110652659377ff
Robert Soriano <sorianorobertc@gmail.com>
2022-08-07 11:37:22 -0700

    bump deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {},
